f3e1c2129fb17e91b2c4a2ee17fcef3efbab49f6
Amber <woll.ambermadison@gmail.com>
2025-04-03 15:34:48 -0600

    commit stashed

--- frontend/src/api/ItemApi.ts
+++ frontend/src/api/ItemApi.ts
@@ -1,2 +1,2 @@
-const url =
-  'https://intex-2-1-backend-brh0g6hbeqhybcb4.eastus-01.azurewebsites.net/';
+// const url =
+//   'https://intex-2-1-backend-brh0g6hbeqhybcb4.eastus-01.azurewebsites.net/';
