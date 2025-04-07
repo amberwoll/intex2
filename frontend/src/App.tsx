6eb8a25ccae6ab70bc2f65e4d294071d9dfba692
katekk2003 <katejeffries200@gmail.com>
2025-04-07 13:12:32 -0600

    Chages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,27 +6,29 @@ import Movies from './pages/Movies';
 import MovieDetail from './pages/MovieDetail';
 import Privacy from './pages/Privacy';
 import ManageMovies from './pages/ManageMovies';
-import "./App.css";
-import { MainLayout } from "./components/MoviePage/MainLayout";
-import TrendsSection from "./components/MoviePage/TrendsSection";
+import './App.css';
+import { MainLayout } from './components/MoviePage/MainLayout';
+import TrendsSection from './components/MoviePage/TrendsSection';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:id" element={<MovieDetail />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/manage-movies" element={<ManageMovies />} />
-      </Routes>
+      <div>
+        {/* Layout and components you want on every page */}
+        <MainLayout />
+        <TrendsSection />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieDetail />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/manage-movies" element={<ManageMovies />} />
+        </Routes>
+      </div>
     </Router>
-    <div>
-      <MainLayout />
-      <TrendsSection />
-    </div>
   );
 }
 
