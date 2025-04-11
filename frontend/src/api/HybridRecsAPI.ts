d74f7a9d0da4858cfd8e75c038ec80d9ce6c5b27
Amber <woll.ambermadison@gmail.com>
2025-04-10 21:50:45 -0600

    test

--- frontend/src/api/HybridRecsAPI.ts
+++ frontend/src/api/HybridRecsAPI.ts
@@ -1,6 +1,7 @@
 // ✅ HybridRecsAPI.ts
 export const fetchHybridRecommendations = async (showId: string) => {
   const apiUrl = process.env.VITE_API_URL;
+  console.log('api', apiUrl);
   const res = await fetch(`${apiUrl}/Movie/HybridRecommendations/${showId}`, {
     credentials: 'include',
   });
