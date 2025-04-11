a670d37d1991805ff378a76ebe1d0618aa4254c4
Amber <woll.ambermadison@gmail.com>
2025-04-11 08:53:56 -0600

    api change and login

--- frontend/src/api/HybridRecsAPI.ts
+++ frontend/src/api/HybridRecsAPI.ts
@@ -2,9 +2,12 @@
 export const fetchHybridRecommendations = async (showId: string) => {
   const apiUrl = process.env.VITE_API_URL;
   console.log('api', apiUrl);
-  const res = await fetch(`${apiUrl}/Movie/HybridRecommendations/${showId}`, {
-    credentials: 'include',
-  });
+  const res = await fetch(
+    `https://intex21-cza7e5hfc3e5evg3.eastus-01.azurewebsites.net/Movie/HybridRecommendations/${showId}`,
+    {
+      credentials: 'include',
+    }
+  );
 
   if (!res.ok) throw new Error('Failed to fetch hybrid recommendations');
   return await res.json();
